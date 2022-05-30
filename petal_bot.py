c74ecf98dff7a98ef327edf848aefb47f86a3d28
effyn <effyn@tutanota.com>
2022-05-30 12:44:43 -0700

    The bridge works I guess

--- petal_bot.py
+++ petal_bot.py
@@ -30,29 +30,30 @@ class PetalBot:
     self.ws: WebSocketClientProtocol = await websockets.connect(constants.PETAL_SERVER)
     await self.send(type = 'auth-token', name = self.name, token = self.token)
 
-    # async def event_message(message):
-    #   if (message.echo or not message.content): return
-    #   await self.ws.send(json.dumps({
-    #     'type': 'message',
-    #     'body': f'[twitch] {message.author.name}: {message.content}'
-    #   }))
-    # self.twitch_bot.event_message = event_message
+    async def event_message(message):
+      if (message.echo or not message.content or message.content.startswith('[twitch] ')): return
+      await self.ws.send(json.dumps({
+        'type': 'message',
+        'body': f'[twitch] {message.author.name}: {message.content}'
+      }))
+    self.twitch_bot.event_message = event_message
 
-    # async def on_message(message):
-    #   self.discord_bot.command
-    #   if message.channel.id != DISCORD_BRIDGE_CHANNEL_ID or message.author.bot or not message.clean_content:
-    #     return
-    #   await self.ws.send(json.dumps({
-    #     'type': 'message',
-    #     'body': f'[discord] {message.author.display_name}: {message.clean_content}'
-    #   }))
-    # self.discord_bot.on_message = on_message
+    async def on_message(message):
+      if message.channel.id != constants.DISCORD_BRIDGE_CHANNEL_ID or\
+        message.author.bot or not message.clean_content or message.clean_content.startswith('[discord] '):
+        return
+      await self.ws.send(json.dumps({
+        'type': 'message',
+        'body': f'[discord] {message.author.display_name}: {message.clean_content}'
+      }))
+    self.discord_bot.on_message = on_message
 
     async for message in self.ws:
       payload = json.loads(message)
-      if payload.get('type') == 'message' and payload.get('name') != constants.PETAL_NAME:
-        send_str = f'{constants.PETAL_EMOJI} {payload.get("name", "anon")}: {payload.get("body")}'
+      name, body = payload.get('name'), payload.get('body')
+      if payload.get('type') == 'message' and name != constants.PETAL_NAME:
+        bridge_str = f'{constants.PETAL_EMOJI} {name or "anon"}: {body}'
         await asyncio.gather(
-          self.twitch_bot.get_channel(constants.BROADCASTER_CHANNEL).send(send_str),
-          self.discord_bot.get_channel(constants.DISCORD_BRIDGE_CHANNEL_ID).send(send_str)
+          self.twitch_bot.get_channel(constants.BROADCASTER_CHANNEL).send(bridge_str),
+          self.discord_bot.get_channel(constants.DISCORD_BRIDGE_CHANNEL_ID).send(bridge_str)
         )
